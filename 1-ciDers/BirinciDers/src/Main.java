13fe338066989684d209b93e2c4def41e144f087
nadjafov05 <nadjafovp@gmail.com>
2022-11-19 04:28:01 +0300

    12

--- 1-ciDers/BirinciDers/src/Main.java
+++ 1-ciDers/BirinciDers/src/Main.java
@@ -24,6 +24,24 @@ public class Main {
         double u = 5;
         double bolme = t / u;
         System.out.println(bolme);
+
+
+        //char: symvol tipidir
+        //char tipinde 1 herf qebul olunur ve bir dirnaq arasinda yazilir.
+
+        char y = 's';
+        System.out.println(y);
+
+
+        //String tipi: symvollar toplusudur, iki dirnaq arasinda yazilir,istediyimiz qeder soz yaza bilerik.
+        String soz = "salam";
+        System.out.println(soz);
+
+
+        //Boolean: true(he) veya false(yox) saxlayir ozunde, misal corek var? true(he) yada false(yox) deye bilerik.
+        boolean b1 = true;
+        System.out.println(b1);
+
         //println bize ekrana capa vermeyimiz ucun lazimdir.
         System.out.println("hello world");
     }
